f191077b0060d91f81026cfea916a7ea4440fa32
Jonathan King <jonking93@gmail.com>
2022-10-04 16:06:29 -0700

    Began documenting proxy data formatting

--- data/proxy_records/proxyData.m
+++ data/proxy_records/proxyData.m
@@ -2,25 +2,80 @@ classdef proxyData
 %% proxyData  Organizes proxy datasets and implements time averaging over Pliocene time slices
 % ----------
 %   The proxyData class provides several methods that help organize and
-%   format the proxy data sets in preparation for data assimilation. The
-%   key method here is "proxyData.organize". This method scans through the
-%   various CSV files, collecting proxy values and metadata. The method
-%   then averages proxy values within the time slices for the assimilation.
-%   Finally, it exports proxy metadata and time-averaged values to a
-%   MAT-file for use with the DASH toolbox.
+%   format the proxy data sets in preparation for data assimilation.
 %
-%   To use this class, call 
+%   **KEY METHOD**
+%   The key method is "proxyData.organize". Running this command will
+%   format the raw proxy records in preparation for assimilation. The
+%   method scans through the proxy record CSV files, collecting proxy
+%   values and metadata. The method then averages proxy values within the
+%   assimilation time slices. Finally, the method exports proxy metadata
+%   and time-averaged values to a NetCDF file for use with the DASH
+%   toolbox. See its help section for additional details.
+%
+%   **EXPERIMENTAL PARAMETERS**
+%   This class includes parameters that set the time-slices used for the
+%   assimilation. (These parameters are used to implement the
+%   time-averaging for the proxy data). You can edit these parameters to
+%   change the time-slices for the assimilation. To edit these parameters,
+%   navigate to the "Experimental Parameters" property block (immediately
+%   after this comment) and follow the prompts.
 
-    % Experimental parameters for time averaging proxy values
+    %% Experimental parameters
+    %
+    % These properties set the time-slices used for the assimilation. You
+    % can edit them to change the time slices for the assimilation. See the
+    % description of each property for instructions on editing.
+    %
+    % If you edit these parameters, you should run the
+    % "proxyData.validateParameters" command in the console to ensure that
+    % your edits are valid.
     properties(Constant)
-        times = [0, 3.25, 4.75];        
+
+        % This indicates the number of time steps in the assimilation. It
+        % should be a scalar positive integer.
+        nTime = 3;
+
+        % This indicates the time metadata to use for each of the
+        % assimilation time steps. It will be saved as metadata in the
+        % NetCDF file containing the formatted proxy data. It should be a
+        % numeric vector with one element per assimilation time step.
+        % It cannot include repeated, NaN, or complex-valued elements.
+        times = [0; 3.25; 4.75];
+
+        % This indicates the units of the time data. It will be saved as
+        % metadata in the NetCDF file containing the formatted proxy data.
+        % It should be a string scalar.
         timeUnits = "Ma";
-        timeBounds = [0 0;3 3.5;4.5 5.5];
-        nRound = 1;  
+
+        % This indicates the spans of ages to use when time-averaging the
+        % proxies for the assimilation time slices. Proxy values within
+        % each span are averaged into the final value for the associated
+        % assimilation time slice.
+        %
+        % This should be a numeric matrix. It should have one row per
+        % assimilation time slice, and two columns. Each row is the time
+        % span for the associated time slice (listed in the "times"
+        % property above). The first column is the lower bound (inclusive)
+        % for the time slice, and the second column is the upper bound
+        % (inclusive). The time span should contain the metadata value used
+        % for the time slice (listed in the "times" property).
+        timeBounds = [0    0
+                      3    3.5
+                      4.5  5.5];
+
+        % Indicates the amount of rounding to apply to the ages in the raw
+        % proxy data files. The property lists the number of digits after
+        % the decimal place to use when rounding. This should be a scalar
+        % integer. Alternatively, you can set this value to NaN to disable
+        % rounding.
+        nRound = 1;
     end
 
 
-    % Data properties scanned from individual data files
+    %% Scanned data properties
+    % This properties are the values scanned from the raw data files. You
+    % probably shouldn't edit them...
     properties (SetAccess = private)
         name;
         lat;
@@ -37,7 +92,34 @@ classdef proxyData
         flags;
     end
 
-    methods
+
+    methods (Static)
+        function[] = organize(folders, saveFile)
+            nFolders = numel(folders);
+            data = cell(nFolders, 1);
+
+            for f = 1:nFolders
+                folder = folders(f);
+                files = proxyData.csvFiles(folder);
+                files = fullfile(folder, files);
+                data{f} = proxyData(files, folder)';
+            end
+            data = [data{:}]';
+
+            data = data.screenFlags;
+            data = data.averageValues;
+            data.export(saveFile);
+        end
+
+
+
+
+
+
+
+
+
+
         function[obj] = proxyData(filenames, defaultType)
             if ~exist('defaultType', 'var')
                 defaultType = string(NaN);
@@ -145,22 +227,7 @@ classdef proxyData
             files = string({files.name});
             files = files(endsWith(files, '.csv'));
         end
-        function[] = organize(folders, saveFile)
-            nFolders = numel(folders);
-            data = cell(nFolders, 1);
 
-            for f = 1:nFolders
-                folder = folders(f);
-                files = proxyData.csvFiles(folder);
-                files = fullfile(folder, files);
-                data{f} = proxyData(files, folder)';
-            end
-            data = [data{:}]';
-
-            data = data.screenFlags;
-            data = data.averageValues;
-            data.export(saveFile);
-        end
     end
 
 end
