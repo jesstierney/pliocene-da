7927a5a8e2a344f1cb9e9fcdb19458cf677df52e
Jonathan King <jonking93@gmail.com>
2022-11-09 14:15:22 -0700

    Create climatologyRange.m

---
+++ data/model_output/raw/code/+load/climatologyRange.m
@@ -0,0 +1,56 @@
+function[X, file] = climatologyRange(filePattern, variable, layer)
+
+% Error check
+assert(isstring(filePattern) && isscalar(filePattern), 'filePattern must be a string scalar');
+assert(isstring(variable) && isscalar(variable), 'variable must be a string scalar');
+
+% Parse the layer
+hasLayers = false;
+if exist('layer','var')
+    assert(isnumeric(layer) && isscalar(layer) && mod(layer,1)==0 && layer>0, 'Layer must be a scalar positive integer');
+    hasLayers = true;
+end
+
+% Check the first file exists
+file = sprintf(filePattern, 1);
+if isempty(which(file))
+    error('Could not find file:\n%s', file);
+end
+
+% Check the variable is in the first file
+info = ncinfo(file);
+fileVariables = {info.Variables.Name};
+[infile, v] = ismember(variable, fileVariables);
+assert(infile, 'File\n%s\ndoes not have a "%s" variable', file, variable);
+
+% Check for the correct number of dimensions
+nDims = numel(info.Variables(v).Dimensions);
+if hasLayers
+    assert(nDims==4, 'You cannot specify a layer because variable "%s" does not have 4 dimensions', variable);
+else
+    assert(nDims==3, 'You did not specify a layer, so variable "%s" must have 3 dimensions', variable);
+end
+
+% Check time has 1 element
+siz = [info.Variables(v).Dimensions.Length];
+assert(siz(end)==1, 'The time dimension of variable "%s" does not have 1 element per file', variable);
+
+% Preallocate
+X = NaN(siz(1), siz(2), 12);
+
+% Get each file
+for month = 1:12
+    file = sprintf(file, month);
+
+    % Load
+    if hasLayers
+        start = [1 1 layer 1];
+        count = [Inf Inf 1 Inf];
+    else
+        start = [1 1 1];
+        count = [Inf Inf Inf];
+    end
+    X(:,:,month) = ncread(file, variable, start, count);
+end
+
+end
