1cc464fa8ac0199bade0f2e682984d120bdb5a8a
Jonathan King <jonking93@gmail.com>
2022-11-09 13:29:56 -0700

    preprocessed COSMOS

---
+++ data/model_output/raw/COSMOS/COSMOS_e280.m
@@ -0,0 +1,46 @@
+function[] = COSMOS_e280
+%% COSMOS_e280  Pre-processes the data for the COSMOS E280 run
+% ----------
+%   COSMOS_e280
+%   Loads variables from the raw output data files, computes climatologies,
+%   and regrids variables to 1x1 resolution. Exports pre-processed
+%   variables to NetCDF.
+% ----------
+%   Outputs:
+%       Creates a NetCDF file named "COSMOS_e280.nc" in the current
+%       directory.
+
+% Set exported NetCDF file
+exportFile = "COSMOS_e280.nc";
+
+% Note characteristics of raw output
+names = ["pr","tas","tos","sos","sic"];
+istripolar = [false, false, false, false, false];
+lonNames = ["lon","lon","lon","lon","lon"];
+latNames = ["lat","lat","lat","lat","lat"];
+conversions = [NaN, 273.15, NaN, NaN, NaN];
+conversionTypes = ["", "+", "", "", ""];
+
+% Get the files / file patterns. 
+files = [...
+    "E280.TotalPrecip_CMIP6_name_pr_2650-2749_monthly_mean_time_series.nc";
+    "E280.NearSurfaceAirTemp_CMIP6_name_tas_2650-2749_monthly_mean_time_series.nc";
+    "E280.SeaSurfaceTemp_CMIP6_name_tos_2650-2749_monthly_mean_time_series.nc";
+    "E280.SeaSurfaceSalinity_CMIP6_name_sos_2650-2749_monthly_mean_time_series.nc";
+    "E280.SeaIceAreaFraction_CMIP6_name_sic_2650-2749_monthly_mean_time_series.nc";
+    ];
+
+% Load the variables
+pr = load.monthly(files(1), names(1));
+tas = load.monthly(files(2), names(2));
+tos = load.monthly(files(3), names(3));
+sos = load.monthly(files(4), names(4));
+siconc = load.monthly(files(5), names(5));
+
+% Process the variables and export to NetCDF
+variables = {pr, tas, tos, sos, siconc};
+variables = processVariables(variables, istripolar, files, lonNames, latNames, conversions, conversionTypes);
+[pr, tas, tos, sos, siconc] = variables{:};
+exportNetCDF(exportFile, pr, tas, tos, sos, siconc);
+
+end
