0ed5032fe999976a318de1992a321e92035fbfce
Jonathan King <jonking93@gmail.com>
2022-11-15 16:02:53 -0700

    Update runAnalysis.m
    Add proxy estimation and R values

--- analysis/runAnalysis.m
+++ analysis/runAnalysis.m
@@ -1,22 +1,28 @@
-function[] = runAnalysis
-
-% Move to the location of this file, but return to original location when
-% the function is exited
-here = mfilename('fullpath');
-home = pwd;
-goback = onCleanup( @()cd(home) );
-cd(here);
 
 % Build the gridfiles
-modelFolder = '../data/model_output/preprocessed';   % Relative to the "runAnalysis" file
-proxyFolder = '../data/proxy_records';               % Relative to the "runAnalysis" file
+modelFolder = '../../data/model_output/preprocessed';   % Relative to the "runAnalysis" file
+proxyFolder = '../../data/proxy_records';               % Relative to the "runAnalysis" file
 buildModelGridfiles(modelFolder);
 buildProxyGridfile(proxyFolder);
 
 % Build the state vector ensembles
-piRuns = ["e280", "preind", "cheyctrl"];
-plioRuns = ["eoi400","eo400new","pi400","plio","pliob17", "cheyco2", ...
-                          "n05","n10","n15","n20","p05","p10","p15","p20"];
+piRuns = parameters.preindustrialRuns;
+plioRuns = parameters.plioceneRuns;
 buildEnsemble('preindustrial', piRuns);
 buildEnsemble('pliocene', plioRuns);
+
+% Download the PSMs
+PSM.download('bayspar');
+PSM.download('bayspline');
+PSM.download('baymag');
+
+% Generate the proxy estimates
+estimateProxies('preindustrial', 0, 'preindustrial', 'lat', 'lon');
+estimateProxies('early-pliocene', 3.25, 'pliocene', 'pLat325', 'pLon325');
+estimateProxies('late-pliocene', 4.75, 'pliocene', 'pLat475', 'pLon475');
+
+% Compute proxy error-variances
+calculateR;
+
+% Run the assimilation
 
