e657f3726a393dcf696e7e59ec4177de61a8687c
Jonathan King <jonking93@gmail.com>
2022-11-21 07:58:15 -0700

    Update buildEnsemble.m
    add error checking

--- ensembles/buildEnsemble.m
+++ ensembles/buildEnsemble.m
@@ -1,7 +1,55 @@
-function[] = buildEnsemble(label, experiments)
+function[] = buildEnsemble(label, runs)
+%% buildEnsemble  Builds an ensemble from a set of runs and saves the ensemble to a .ens file
+% ----------
+%   buildEnsemble(label, runs)
+%   Builds a state vector ensemble from a collection of model runs. The
+%   ensemble is saved in a .ens file in the current directory. The name of 
+%   the .ens file will match the label provided for the ensemble.
+% 
+%   The state vector contains variables for the "pr", "tas", "tos", and 
+%   "siconc" reconstruction targets for annual, DJF, and JJA seasonal means.
+%   The names of the variables follow the naming convention: 
+%       <climate variable>_<season>
+%
+%   As a reminder, variables are as follows:
+%       pr - Total precipitation
+%       tas - Near surface air temperature
+%       tos - Sea surface temperature
+%       sos - Sea surface salinity
+%       siconc - Sea ice area percentage
+%
+%   Seasons are as follows:
+%       annual - Annual mean
+%       DJF    - Boreal Winter (December-January-February)
+%       JJA    - Boreal Summer (June-July-August)
+%
+%   The state vector also includes monthly "tos" and "sos" variables, which
+%   are used to run the PSMs. These variables are named "tos_monthly" and
+%   "sos_monthly".
+%
+%   The ensemble will contain one ensemble member per indicated run. This
+%   method will throw an error if a run is not in the gridfile, or if the
+%   list of runs contains duplicate values.
+% ----------
+%   Inputs:
+%       label (string scalar): A label for the ensemble. This label will be
+%           used as the file name for the saved ".ens" file.
+%       runs (string matrix [nRuns x 2]): Metadata for the runs that should
+%           be included in the ensemble. The first column lists the climate
+%           model associated with each run, and the second column lists the
+%           experiment tags.
+%
+%   Outputs:
+%       Creates a file named <label>.ens in the current directory. The file
+%       holds the saved state vector ensemble.
 
-% Create state vector
-sv = stateVector(label);
+% Error check
+if ~((isstring(label)&&isscalar(label)) || (ischar(label)&&isrow(label)))
+    error('label must be a string scalar');
+end
+assert(isstring(runs)&&ismatrix(runs)&&size(runs,2)==2, 'runs must be a string matrix with 2 columns');
+uniqueRuns = unique(runs, 'rows');
+assert(size(uniqueRuns,1)==size(runs,1), 'runs cannot contain duplicate rows');
 
 % Get gridfiles
 pr = gridfile("pr");
@@ -9,6 +57,17 @@ tas = gridfile("tas");
 tos = gridfile("tos");
 sos = gridfile("sos");
 siconc = gridfile("siconc");
+
+% Check each run is in the gridfile
+allRuns = tos.metadata.run;
+missing = ~ismember(runs, allRuns, 'rows');
+if any(missing)
+    missing = find(missing, 1);
+    error('Run %.f is not in the gridfile\n\tModel: %s\n\tExperiment: %s', missing, runs(missing,1), runs(missing,2));
+end
+
+% Create state vector
+sv = stateVector(label);
 
 % Add variables for reconstruction targets
 grids = [pr, tas, tos, siconc];
@@ -33,8 +92,7 @@ sv = sv.add("tos_monthly", tos);
 sv = sv.add("sos_monthly", sos);
 
 % Select from pre-industrial runs
-allExperiments = tos.metadata.run(:,2);
-use = ismember(allExperiments, experiments);
+use = ismember(allRuns, runs, 'rows');
 sv = sv.design(-1, 'run', 'ensemble', use);
 
 % Build the ensemble
